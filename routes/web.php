cc93de583698a1d35587cf418a8521bfafe1f7ef
aderonirpl <aderoni@smkn1padaherang.sch.id>
2024-05-31 07:09:55 +0700

    role permission

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LearningController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->name('dashboard')->group(function () {
+        
+        Route::resource('courses', CourseController::class)->middleware('role:teacher');
+
+        Route::get('/learning', [LearningController::class, 'index'])->middleware('role:student')->name('learning.index'); ;
+
+    });
+});
+
+require __DIR__.'/auth.php';
